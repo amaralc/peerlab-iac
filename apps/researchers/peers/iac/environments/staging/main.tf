6b3bedcadb087f1fcf8e5e631c931714522b7644
amaralc <amaral.calil@gmail.com>
2023-05-20 22:56:07 -0300

    adjust: use full commit hash

--- apps/researchers/peers/iac/environments/staging/main.tf
+++ apps/researchers/peers/iac/environments/staging/main.tf
@@ -175,7 +175,7 @@ locals {
 # Fetch the current commit hash and write it to a file
 resource "null_resource" "get_commit_hash" {
   provisioner "local-exec" {
-    command = "git rev-parse --short HEAD > ${local.commit_hash_file}" // TODO: Use full commit hash?
+    command = "git rev-parse HEAD > ${local.commit_hash_file}"
   }
 }
 

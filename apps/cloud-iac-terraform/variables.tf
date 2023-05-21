27865e5b6a07d0d015baff4e04e167b68b48c3f3
amaralc <amaral.calil@gmail.com>
2023-05-21 17:39:59 -0300

    adjust: add hash and old config

--- apps/cloud-iac-terraform/variables.tf
+++ apps/cloud-iac-terraform/variables.tf
@@ -1,4 +1,14 @@
+# variable "database_url" {
+#   description = "The database URL connection string"
+#   type        = string
+#   sensitive   = true
+# }
 
+# variable "direct_url" {
+#   description = "The direct URL string"
+#   type        = string
+#   sensitive   = true
+# }
 
 # Define the variables that will be used within the configuration
 variable "project_id" {
@@ -9,6 +19,17 @@ variable "project_id" {
 variable "region" {
   description = "The region where resources will be created"
   type        = string
+
+}
+variable "repo_owner" {
+  description = "The GitHub owner's username"
+  type        = string
+  default     = "amaralc"
+}
+variable "repo_name" {
+  description = "The name of the source repository"
+  type        = string
+  default     = "peerlab"
 }
 
 variable "credentials_path" {
@@ -20,16 +41,4 @@ variable "credentials_path" {
 variable "environment" {
   description = "The deployment environment (staging, production, etc.)"
   type        = string
-}
-
-variable "database_url" {
-  description = "The database URL connection string"
-  type        = string
-  sensitive   = true
-}
-
-variable "direct_url" {
-  description = "The direct URL string"
-  type        = string
-  sensitive   = true
 }
